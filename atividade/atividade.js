5d0b7a5e1e89488ad404cf228cee7e571b708a1f
Isadora <162151148+1sadora08@users.noreply.github.com>
2025-04-29 14:41:39 -0300

    Add files via upload

---
+++ atividade/atividade.js
@@ -0,0 +1,17 @@
+function cadastrarNota(aluno, nota){
+let aluno = ("Digite seu nome:")    
+let nota1 = ("Digite sua 1 nota:");
+let nota2 = ("Digite sua 2 nota:");}
+
+function calcularMedia(aluno){
+return (nota1 + nota2) / 2}
+
+function verificarAprovação(media){
+let media = calcularMedia
+}
+
+function exibirResultado(aluno, media, aprovado){
+  if(aprovado){media < 6 == ("Não aprovado!")}
+else {media > 6 == ("Aprovado!")}
+("Aluno "+aluno+", sua média é "+media+" e você foi:" +aprovado)
+}
